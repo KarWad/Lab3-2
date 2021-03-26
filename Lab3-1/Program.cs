5759ac98e4dcce0e4dd18fb6db2fd36f2b4d7d48
KarWad <phinb@LAPTOP-25LHGEP6.(none)>
2021-03-26 15:08:34 +0100

    Dodaj pliki projektów.

---
+++ Lab3-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3_1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dick = new Dictionary<string, string>();
+            dick.Add("001", "David");
+            dick.Add("002", "Karolina");
+            dick.Add("003", "Goncalo");
+            System.Console.WriteLine("One of my best friend is: {0}", dick["001"] + ".");
+        }
+    }
+}
